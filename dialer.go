139e0f24e83f25125ee257a446a78a70135c51f0
Lorenzo Manacorda <lorenzo@mailbox.org>
2016-10-24 12:52:15 +0200

    pass number of connections as argument

--- dialer.go
+++ dialer.go
@@ -4,22 +4,29 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 )
 
 func main() {
-	if len(os.Args) != 2 {
+	if len(os.Args) != 3 {
 		fmt.Println("Not enough arguments")
 		os.Exit(1)
 	}
 
 	url := os.Args[1]
+	numConn, err := strconv.Atoi(os.Args[2])
+	if err != nil {
+		fmt.Printf("Error with second argument\n")
+		os.Exit(1)
+	}
 
-	fmt.Printf("Connecting to %s\n", url)
-	for x := 0; x < 10; x++ {
+	fmt.Printf("Establishing %d TCP connections to %s\n", numConn, url)
+	for x := 0; x < numConn; x++ {
 		_, err := net.Dial("tcp", url)
 
 		if err != nil {
 			fmt.Printf("Error: %v\n", err)
+			os.Exit(1)
 		}
 	}
 
