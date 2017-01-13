3fb7993daaf8c15907b6e71639d38ade0d2830da
Alban Crequy <alban@kinvolk.io>
2017-01-13 17:04:05 +0100

    creating short-lived connections
    Use it this way:
    
    $ ./dialer connectshortlived 127.0.0.1:4041
    Establishing 1 short-lived TCP connection every second to 127.0.0.1:4041
    127.0.0.1:52768
    127.0.0.1:52770
    127.0.0.1:52772
    127.0.0.1:52776
    127.0.0.1:52778
    
    Every second, it connects, prints the local address, and close the
    connection.

--- dialer.go
+++ dialer.go
@@ -5,6 +5,7 @@ import (
 	"net"
 	"os"
 	"strconv"
+	"time"
 )
 
 func connect(url string, numConn int) {
@@ -20,6 +21,21 @@ func connect(url string, numConn int) {
 
 	// wait forever
 	select {}
+}
+
+func connectshortlived(url string) {
+	fmt.Printf("Establishing 1 short-lived TCP connection every second to %s\n", url)
+	for {
+		conn, err := net.Dial("tcp", url)
+
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("%v\n", conn.LocalAddr())
+		conn.Close()
+		time.Sleep(time.Second)
+	}
 }
 
 func listen(url string) {
@@ -69,6 +85,16 @@ func main() {
 		}
 
 		connect(url, numConn)
+	}
+	if verb == "connectshortlived" {
+		if len(os.Args) != 3 {
+			fmt.Println("Not enough arguments")
+			os.Exit(1)
+		}
+
+		url := os.Args[2]
+
+		connectshortlived(url)
 	}
 	if verb == "listen" {
 		if len(os.Args) != 3 {
